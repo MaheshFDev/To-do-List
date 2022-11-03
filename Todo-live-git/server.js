eb8a53470b31d33acfbd5a358204c56f99e8d1bf
MaheshFDev <72066046+MaheshFDev@users.noreply.github.com>
2022-11-03 17:43:50 +0530

    Changed the title

--- Todo-live-git/server.js
+++ Todo-live-git/server.js
@@ -26,7 +26,7 @@ app.get('/', function (req, res) {
     </head>
     <body>
       <div class="container">
-        <h1 class="display-4 text-center py-1">To-Do App</h1>
+        <h1 class="display-4 text-center py-1">To-Do Live App</h1>
         
         <div class="jumbotron p-3 shadow-sm">
           <form id="ourForm" action="/create-item" method="POST">
